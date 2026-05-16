c39672b817434cdddda8d301e018a1fd2b07c47a
wojtkoszyk-dot <wojtkoszyk@gmail.com>
2026-05-16 07:54:03 +0200

    Update next.config.js
    mvp fix

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
